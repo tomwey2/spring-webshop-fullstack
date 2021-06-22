c72bc630821d848ece28fc2c321cde1174fa2d3b
Thomas Weyrath <tomwey2@gmail.com>
2021-06-22 17:14:33 +0200

    added id in cart_content table

--- src/db/carts.sql
+++ src/db/carts.sql
@@ -9,9 +9,11 @@ CREATE TABLE carts (
 
 DROP TABLE IF EXISTS cart_content;
 CREATE TABLE cart_content (
+  content_id        INT         NOT NULL AUTO_INCREMENT,
   cart_id           INT         NOT NULL,
   product_id        INT         NOT NULL,
-  quantity          INT         DEFAULT 0
+  quantity          INT         DEFAULT 0,
+  PRIMARY KEY (content_id)
 );
 
 INSERT INTO carts (customer_id, create_date)
