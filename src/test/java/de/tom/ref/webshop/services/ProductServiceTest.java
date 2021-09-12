357680c019baf55eb1a0412303cdb31b3a6a14cb
Thomas <tomwey2@gmail.com>
2021-09-12 17:11:42 +0200

    implemented junit tests for class ProductService

--- src/test/java/de/tom/ref/webshop/services/ProductServiceTest.java
+++ src/test/java/de/tom/ref/webshop/services/ProductServiceTest.java
@@ -1,19 +1,18 @@
 package de.tom.ref.webshop.services;
 
+import de.tom.ref.webshop.WebshopApplication;
 import de.tom.ref.webshop.entities.Product;
 import de.tom.ref.webshop.entities.ProductCategory;
+import de.tom.ref.webshop.repositories.ProductCategoryRepository;
 import de.tom.ref.webshop.repositories.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,63 +22,99 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ExtendWith(MockitoExtension.class)
 @Slf4j
 public class ProductServiceTest {
-    private String separator = "##### Execute test: {} #####";
+    private final String separator = "##### Execute test: {} #####";
 
-    @Mock
-    private ProductRepository productRepository;
-    @InjectMocks
-    private ProductService productService;
+    private final ProductRepository productRepository;
+    private final ProductCategoryRepository productCategoryRepository;
+    private final ProductCategoryService productCategoryService;
+    private final ProductService productService;
 
-    private ProductCategory productCategory1;
-    private ProductCategory productCategory2;
-    private List<Product> products;
+    private List<ProductCategory> testCategories;
+    private List<Product> testProducts;
+
+    public ProductServiceTest() {
+        // create the mocked classes
+        productRepository = Mockito.mock(ProductRepository.class);
+        productCategoryRepository = Mockito.mock(ProductCategoryRepository.class);
+        // instantiate the service classes
+        productCategoryService = new ProductCategoryService(productCategoryRepository);
+        productService = new ProductService(productRepository, productCategoryService);
+    }
 
     @BeforeEach
     public void beforeEach() {
-        productCategory1 = new ProductCategory("Category 1");
-        productCategory1.setId(1);
-        productCategory2 = new ProductCategory("Category 2");
-        productCategory2.setId(2);
-
-        products = new ArrayList<>();
-        products.add(new Product("Product 1", productCategory1, BigDecimal.ZERO, 0));
-        products.add(new Product("Product 2", productCategory1, BigDecimal.ZERO, 0));
-        products.add(new Product("Product 3", productCategory2, BigDecimal.ZERO, 0));
+        testCategories = WebshopApplication.initProductCategories();
+        int index = 1;
+        for (ProductCategory category: testCategories) {
+            category.setId(index);
+            index++;
+        }
+        testProducts = WebshopApplication.initProducts(testCategories);
+        index = 1;
+        for (Product product: testProducts) {
+            product.setId(index);
+            index++;
+        }
     }
 
     @Test
     void getProducts() {
         log.debug(separator, "Test getProducts()");
-        Mockito.when(productRepository.findAll()).thenReturn(products);
+        Mockito.when(productRepository.findAll()).thenReturn(testProducts);
 
-        assertEquals(3, productService.getProducts(null).size()); // get all products
-        assertEquals(3, productService.getProducts(0).size()); // get all products
+        assertEquals(testProducts.size(), productService.getProducts(null).size()); // get all products
+        assertEquals(testProducts.size(), productService.getProducts(0).size()); // get all products
         assertEquals(2, productService.getProducts(1).size());
-        assertEquals(1, productService.getProducts(2).size());
-        assertEquals(0, productService.getProducts(3).size()); // list is empty if the category not exists
+        assertEquals(3, productService.getProducts(2).size());
+        assertEquals(0, productService.getProducts(5).size()); // list is empty if the category not exists
     }
 
     @Test
-    void addProduct() {
-        Product newProduct = new Product("new Product", productCategory1, BigDecimal.ZERO, 0);
-        newProduct.setId(15);
-        Mockito.when(productRepository.findProductByName("new Product")).thenReturn(Optional.empty());
-        Mockito.when(productRepository.save(newProduct)).thenReturn(newProduct);
+    void addNewProduct() {
+        log.debug(separator, "Test addNewProduct()");
+        String newProductName = "new Product";
+        Product newProduct = new Product(newProductName, null, BigDecimal.ZERO, 0);
+        Integer categoryId = 1;
+        ProductCategory category = new ProductCategory("Test Category");
+        category.setId(categoryId);
+        Mockito.when(productRepository.findByName(newProductName))
+                .thenReturn(null);
+        Mockito.when(productRepository.save(newProduct))
+                .thenReturn(newProduct);
+        Mockito.when(productCategoryRepository.findById(categoryId))
+                .thenReturn(Optional.of(category));
 
-        Product result1 = productService.addProduct(newProduct);
-        assertEquals(15, result1.getId());
+        Product result1 = productService.addProduct(categoryId, newProduct);
+        assertEquals(categoryId, result1.getCategory().getId());
+    }
 
-        // if the name of the new product already exists then an exception is thrown
-        newProduct.setName("Product 2");
-        Mockito.when(productRepository.findProductByName("Product 2")).thenReturn(Optional.of(products.get(1)));
-        assertThrows(IllegalStateException.class, () -> {productService.addProduct(newProduct);});
+    @Test
+    void addExistingProduct() {
+        log.debug(separator, "Test addExistingProduct()");
+        String existingProductName = "existing Product";
+        Product existingProduct = new Product(existingProductName, null, BigDecimal.ZERO, 0);
+        Integer categoryId = 1;
+        ProductCategory category = new ProductCategory("Test Category");
+        category.setId(categoryId);
+        Mockito.when(productRepository.findByName(existingProductName))
+                .thenReturn(existingProduct);
+        Mockito.when(productCategoryRepository.findById(categoryId))
+                .thenReturn(Optional.of(category));
 
+        assertThrows(IllegalStateException.class, () -> {
+            productService.addProduct(categoryId, existingProduct);
+        });
     }
 
     @Test
     void delProduct() {
+        log.debug(separator, "Test delProduct()");
         Mockito.when(productRepository.existsById(1)).thenReturn(false);
-        assertThrows(IllegalStateException.class, () -> {productService.delProduct(1);});
-        assertThrows(IllegalStateException.class, () -> {productService.delProduct(null);});
+        assertThrows(IllegalStateException.class, () -> {
+            productService.delProduct(1);
+        });
+        assertThrows(IllegalStateException.class, () -> {
+            productService.delProduct(null);
+        });
     }
 }
